43d669405684cfcb504030c073172fd348c48636
tar-xzvff <jv120228m@gmail.com>
2020-12-21 19:38:01 +0900

    add query

--- nvidia_smi_exporter.go
+++ nvidia_smi_exporter.go
@@ -18,7 +18,7 @@ import (
 func metrics(response http.ResponseWriter, request *http.Request) {
     out, err := exec.Command(
         "nvidia-smi",
-        "--query-gpu=name,index,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used",
+        "--query-gpu=name,index,timestamp,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used,clocks_throttle_reasons.sw_power_cap,clocks_throttle_reasons.sw_thermal_slowdown,clocks.current.graphics,temperature.gpu,temperature.memory,power.draw",
         "--format=csv,noheader,nounits").Output()
 
     if err != nil {
