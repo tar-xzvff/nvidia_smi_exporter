0bb12e6fcada5fd9b161f0c854732f9b5b31c644
tar-xzvff <jv120228m@gmail.com>
2020-12-21 21:22:40 +0900

    add metric

--- nvidia_smi_exporter.go
+++ nvidia_smi_exporter.go
@@ -18,7 +18,7 @@ import (
 func metrics(response http.ResponseWriter, request *http.Request) {
     out, err := exec.Command(
         "nvidia-smi",
-        "--query-gpu=name,index,timestamp,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used,clocks_throttle_reasons.sw_power_cap,clocks_throttle_reasons.sw_thermal_slowdown,clocks.current.graphics,temperature.memory,power.draw",
+        "--query-gpu=name,index,timestamp,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used,clocks_throttle_reasons.sw_power_cap,clocks_throttle_reasons.sw_thermal_slowdown,clocks.current.graphics,temperature.memory,power.draw,driver_version,count,gpu_serial,pcie.link.gen.current,pcie.link.gen.max,pcie.link.width.current,pcie.link.width.max,fan.speed,pstate,clocks_throttle_reasons.supported,clocks_throttle_reasons.active,clocks_throttle_reasons.gpu_idle,clocks_throttle_reasons.applications_clocks_setting,clocks_throttle_reasons.hw_slowdown,clocks_throttle_reasons.hw_thermal_slowdown,clocks_throttle_reasons.hw_power_brake_slowdown,clocks_throttle_reasons.sync_boost,power.limit,enforced.power.limit,power.default_limit,power.min_limit,power.max_limit,clocks.current.sm,clocks.current.memory,clocks.current.video",
         "--format=csv,noheader,nounits").Output()
 
     if err != nil {
@@ -40,7 +40,20 @@ func metrics(response http.ResponseWriter, request *http.Request) {
         "temperature.gpu", "utilization.gpu",
         "utilization.memory", "memory.total", "memory.free", "memory.used",
         "clocks_throttle_reasons.sw_power_cap", "clocks_throttle_reasons.sw_thermal_slowdown", "clocks.current.graphics",
-        "temperature.memory", "power.draw"}
+        "temperature.memory", "power.draw", "driver_version","count","gpu_serial","pcie.link.gen.current",
+        "pcie.link.gen.max", "pcie.link.width.current",
+        "pcie.link.width.max", "fan.speed",
+        "pstate", "clocks_throttle_reasons.supported",
+        "clocks_throttle_reasons.active",
+        "clocks_throttle_reasons.gpu_idle",
+        "clocks_throttle_reasons.applications_clocks_setting",
+        "clocks_throttle_reasons.hw_slowdown", "clocks_throttle_reasons.hw_thermal_slowdown",
+        "clocks_throttle_reasons.hw_power_brake_slowdown",
+        "clocks_throttle_reasons.sync_boost", "power.limit",
+        "enforced.power.limit", "power.default_limit",
+        "power.min_limit", "power.max_limit",
+        "clocks.current.sm",
+        "clocks.current.memory", "clocks.current.video"}
 
     result := ""
     for _, row := range records {
