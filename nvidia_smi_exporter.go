4febb3803727435ea36acc98071925c357387529
tar-xzvff <jv120228m@gmail.com>
2020-12-22 12:01:00 +0900

    文字列メトリクスを{ } の中に移動

--- nvidia_smi_exporter.go
+++ nvidia_smi_exporter.go
@@ -18,7 +18,7 @@ import (
 func metrics(response http.ResponseWriter, request *http.Request) {
     out, err := exec.Command(
         "nvidia-smi",
-        "--query-gpu=name,index,timestamp,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used,clocks_throttle_reasons.sw_power_cap,clocks_throttle_reasons.sw_thermal_slowdown,clocks.current.graphics,temperature.memory,power.draw,driver_version,count,gpu_serial,pcie.link.gen.current,pcie.link.gen.max,pcie.link.width.current,pcie.link.width.max,fan.speed,pstate,clocks_throttle_reasons.supported,clocks_throttle_reasons.active,clocks_throttle_reasons.gpu_idle,clocks_throttle_reasons.applications_clocks_setting,clocks_throttle_reasons.hw_slowdown,clocks_throttle_reasons.hw_thermal_slowdown,clocks_throttle_reasons.hw_power_brake_slowdown,clocks_throttle_reasons.sync_boost,power.limit,enforced.power.limit,power.default_limit,power.min_limit,power.max_limit,clocks.current.sm,clocks.current.memory,clocks.current.video",
+        "--query-gpu=name,index,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used,clocks_throttle_reasons.sw_power_cap,clocks_throttle_reasons.sw_thermal_slowdown,clocks.current.graphics,temperature.memory,power.draw,driver_version,count,gpu_serial,pcie.link.gen.current,pcie.link.gen.max,pcie.link.width.current,pcie.link.width.max,fan.speed,pstate,clocks_throttle_reasons.supported,clocks_throttle_reasons.active,clocks_throttle_reasons.gpu_idle,clocks_throttle_reasons.applications_clocks_setting,clocks_throttle_reasons.hw_slowdown,clocks_throttle_reasons.hw_thermal_slowdown,clocks_throttle_reasons.hw_power_brake_slowdown,clocks_throttle_reasons.sync_boost,power.limit,enforced.power.limit,power.default_limit,power.min_limit,power.max_limit,clocks.current.sm,clocks.current.memory,clocks.current.video",
         "--format=csv,noheader,nounits").Output()
 
     if err != nil {
@@ -36,7 +36,6 @@ func metrics(response http.ResponseWriter, request *http.Request) {
     }
 
     metricList := []string {
-    	"timestamp",
         "temperature.gpu", "utilization.gpu",
         "utilization.memory", "memory.total", "memory.free", "memory.used",
         "clocks_throttle_reasons.sw_power_cap", "clocks_throttle_reasons.sw_thermal_slowdown", "clocks.current.graphics",
@@ -59,9 +58,27 @@ func metrics(response http.ResponseWriter, request *http.Request) {
     for _, row := range records {
         name := fmt.Sprintf("%s[%s]", row[0], row[1])
         for idx, value := range row[2:] {
-            result = fmt.Sprintf(
-                "%s%s{gpu=\"%s\"} %s\n", result,
-                strings.Replace(metricList[idx], ".", "_", -1), name, value)
+            if metricList[idx] == "clocks_throttle_reasons.sw_power_cap" ||
+		metricList[idx] == "clocks_throttle_reasons.sw_thermal_slowdown" ||
+		metricList[idx] == "clocks_throttle_reasons.sync_boost" ||
+		metricList[idx] == "clocks_throttle_reasons.hw_power_brake_slowdown" ||
+		metricList[idx] == "clocks_throttle_reasons.hw_thermal_slowdown" ||
+		metricList[idx] == "clocks_throttle_reasons.hw_slowdown" ||
+		metricList[idx] == "clocks_throttle_reasons.applications_clocks_setting" ||
+		metricList[idx] == "clocks_throttle_reasons.gpu_idle" ||
+		metricList[idx] == "clocks_throttle_reasons.supported" ||
+		metricList[idx] == "clocks_throttle_reasons.active" ||
+		metricList[idx] == "pstate" ||
+		metricList[idx] == "driver_version" ||
+		metricList[idx] == "fan.speed" {
+                result = fmt.Sprintf(
+                    "%s%s{gpu=\"%s\",status=\"%s\"} %s\n", result,
+                    strings.Replace(metricList[idx], ".", "_", -1), name, value, "1")
+            } else {
+                result = fmt.Sprintf(
+                    "%s%s{gpu=\"%s\"} %s\n", result,
+                    strings.Replace(metricList[idx], ".", "_", -1), name, value)
+            }
         }
     }
     fmt.Fprintf(response, strings.Replace(result, "", "", -1))
